9e1160df2c136d41c5896ecd87abaaf735f6c304
Jet <jetjdsimon@gmail.com>
2023-11-29 11:41:53 -0800

    Update HowTo.js

--- src/components/HowTo.js
+++ src/components/HowTo.js
@@ -4,9 +4,15 @@ function HowTo(props) {
             <div className="HowTo">
             <h3>What is a subanagram?</h3>
             <p>A subanagram is a contiguous subset of a word rearranged to form a new word.<br></br><br></br>Here is an example of one:</p>
-            <div>s<span style={{'backgroundColor':'yellow'}}>ponso</span>rships → <span style={{'backgroundColor':'yellow'}}>ponso</span> → snoop <span style={{'color':'green', 'fontWeight':'boldest'}}>✓</span></div>
-            <p>And this is NOT a subanagram, because the letters need to all be from the same chunk of the word:</p>
-            <div><span style={{'backgroundColor':'yellow'}}>s</span>ponsorsh<span style={{'backgroundColor':'yellow'}}>ips</span> → <span style={{'backgroundColor':'yellow'}}>s ips</span> → sips <span style={{'color':'red', 'fontWeight':'boldest'}}>✗</span></div>
+            <div>
+                <p>s<span style={{'backgroundColor':'yellow'}}>ponso</span>rships</p>
+                <p><span style={{'visibility':'hidden'}}>s</span><span style={{'backgroundColor':'yellow'}}>ponso</span></p>
+                <p>snoop <span style={{'color':'green', 'fontWeight':'boldest'}}>✓</span></p></div>
+            <p>And this is <span style={{'fontWeight':'bold'}}>not</span> a subanagram, because the letters need to all be from the same chunk of the word:</p>
+            <div>
+                <p><span style={{'backgroundColor':'yellow'}}>s</span>ponsorsh<span style={{'backgroundColor':'yellow'}}>ips</span></p>
+                <p><span style={{'backgroundColor':'yellow'}}>s</span><span style={{'visibility':'hidden'}}>ponshors</span><span style={{'backgroundColor':'yellow'}}>ips</span></p>
+                <p>sips <span style={{'color':'red', 'fontWeight':'boldest'}}>✗</span></p></div>
             <button className="button-4" onClick={props.onCloseClicked}>Okay</button>
             </div>
         </div>
